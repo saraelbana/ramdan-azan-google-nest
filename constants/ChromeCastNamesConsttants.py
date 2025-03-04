38b0651e21a967148b0c386aeaa6a00efd2dd648
Sara Elbana <150337030+saraelbana@users.noreply.github.com>
2025-03-04 15:18:02 +0100

    a reference comment to the google nest display/mini friendly name

--- constants/ChromeCastNamesConsttants.py
+++ constants/ChromeCastNamesConsttants.py
@@ -1,6 +1,6 @@
-living_room_display = "Living Room Display"
-bedroom_speaker = "Bedroom speaker"
-upper_living_speaker = "upper living speaker"
+living_room_display = "Living Room Display" # nest friendly name, the one thay is shown in the goolge home app "Living Room Display"
+bedroom_speaker = "Bedroom speaker" # nest friendly name, the one thay is shown in the goolge home app "Bedroom speaker"
+upper_living_speaker = "upper living speaker" # nest friendly name, the one thay is shown in the goolge home app "upper living speaker"
 
 device_names = {
     "livingRoomDisplay": living_room_display,
