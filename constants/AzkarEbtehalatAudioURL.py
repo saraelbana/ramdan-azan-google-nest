71c550aab1aa4e585081f818243ec67cca657487
Sara Elbana <150337030+saraelbana@users.noreply.github.com>
2025-03-04 10:09:27 +0100

    adding mesaharaty, ebtehalat and madfaa aliftar Audio fragments URL in seperates files

---
+++ constants/AzkarEbtehalatAudioURL.py
@@ -0,0 +1,16 @@
+azkar = "https://nuralaanur.com/wp-content/uploads/2023/07/أذكار-الصباح-والمساء-للشيخ-محمد-جبريل.mp3"
+azkar_DURATION = 2490
+# 41 minutes 30 seconds
+ebtehalat = "https://nuralaanur.com/wp-content/uploads/2023/12/ساعة-كاملة-من-الابتهالات-للشيخ-سيد-النقشبندي.mp3"
+ebtehalat_DURATION = 4365
+# 1hour 12 minutes 45 seconds
+asmaaAllahAlhosna = "https://nuralaanur.com/wp-content/uploads/2023/12/أسماء-الله-الحسنى-بصوت-الشيخ-سيد-النقشبندي.mp3"
+asmaaAllahAlhosna_DURATION = 210
+# 3 minutes 30 seconds
+
+sound = {
+    "Azkar": azkar,
+    "Ebtehalat": ebtehalat,
+    "AsmaaAllahAlhosna": asmaaAllahAlhosna
+}
+
---
+++ constants/MadfaaAliftar.py
@@ -0,0 +1,9 @@
+MADFAA_ALIFTAR_AUDIO_URL = "https://archive.org/embed/y-2mate.com-dh-nn-32d-edx-m_202006/y2mate.com - مدفع الأفطار أضرب_dhNN32dEDxM.mp3"
+MADFAA_ALIFTAR_AUDIO_DURATION = 9
+# only plays the audio for 9 seconds, ie the first 9 minutes of the audio file
+MADFAA_ALIFTAR = {
+    "MADFAA_ALIFTAR_AUDIO": {
+        "URL": MADFAA_ALIFTAR_AUDIO_URL,
+        "DURATION": MADFAA_ALIFTAR_AUDIO_DURATION
+    }
+}
